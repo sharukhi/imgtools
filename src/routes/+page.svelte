3bce6b4669d7ac550cca05b78982c5cdbe9be983
sharukhi <sharukhi@sharukhi.xyz>
2024-07-04 22:07:44 +0600

    Update main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,30 @@
 
 <div>
   <div class="h-screen flex items-center justify-center">
-    <div class="grid grid-cols-1 gap-9">
-      <div><a href="/jpg" class="button">Convert to jpg</a></div>
-      <div><a href="/png" class="button">Convert to png</a></div>
-      <div><a href="/webp" class="button">Convert to webp</a></div>
+    <div class="grid grid-cols-2 gap-9 text-5xl">
+      <div>
+        <a href="/jpg" class="selection-menu"
+          ><iconify-icon icon="hugeicons:jpg-01" style="color: #0c3d26"
+          ></iconify-icon></a
+        >
+      </div>
+      <div>
+        <a href="/png" class="selection-menu"
+          ><iconify-icon icon="hugeicons:png-02" style="color: #0c3d26"
+          ></iconify-icon></a
+        >
+      </div>
+      <div>
+        <a href="/webp" class="selection-menu"
+          ><iconify-icon icon="iconoir:webp-format" style="color: #0c3d26"
+          ></iconify-icon></a
+        >
+      </div>
+      <div>
+        <a href="/" class="selection-menu"
+          ><p class="text-lg font-extrabold text-center">Coming soon</p>
+        </a>
+      </div>
     </div>
   </div>
 </div>
