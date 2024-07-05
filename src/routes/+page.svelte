078406e855cd027fcd847d08ea8ffc57319b5b3f
sharukhi <sharukhi@sharukhi.xyz>
2024-07-05 18:01:05 +0600

    Update UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
 <div>
   <div class="h-screen flex items-center justify-center">
-    <div class="grid grid-cols-2 gap-9 text-5xl">
+    <div class="grid grid-cols-3 gap-9 text-5xl">
       <div>
         <a href="/jpg" class="selection-menu"
           ><iconify-icon icon="hugeicons:jpg-01" style="color: #0c3d26"
@@ -26,11 +26,6 @@
           ><iconify-icon icon="iconoir:webp-format" style="color: #0c3d26"
           ></iconify-icon></a
         >
-      </div>
-      <div>
-        <a href="/" class="selection-menu"
-          ><p class="text-lg font-extrabold text-center">Coming soon</p>
-        </a>
       </div>
     </div>
   </div>
